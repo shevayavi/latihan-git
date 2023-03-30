51b3c5017ddce7e6283911eb38730f742263cc2d
shevayavi <128027085+shevayavi@users.noreply.github.com>
2023-03-30 16:50:18 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Imam Nawawi Saya tinggal di daerah ciputat olahraga yang saya sukai adalah bulutangkis";
+    }
+}
